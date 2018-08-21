34360d0c17d197fb97ee5c104c9a1d31183bd500
Daniel Schoch <d.schoch90@gmail.com>
2018-08-21 23:56:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,9 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
-      season[holiday] << supply.capitalize!
+      season.each do |holiday, supplies|
+        holiday[supplies] << supply.capitalize!
+      end
     end
   end
 end
