0af9ca91099b490b250b7bfb98e6a08c1545c4be
Daniel Schoch <d.schoch90@gmail.com>
2018-08-22 00:08:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,8 +33,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  holiday_hash[season.to_sym][holiday_name.to_sym][supply_array]
 
 end
 
